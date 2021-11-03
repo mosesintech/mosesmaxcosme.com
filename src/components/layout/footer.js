cfa9054e186aabe889472ed9b6bf2b924bb58216
Moses Cosme <mosesmaxcosme@gmail.com>
2021-11-03 13:43:07 -0500

    finish homepage, header, and simple blogroll

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -1,33 +1,16 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import moment from 'moment'
+
+const year = moment(new Date()).format('YYYY');
 
 const Footer = ({ siteTitle }) => (
-      <footer class="bg-dark footer-section">
-        <div class="section">
-          <div class="container">
-            <div class="row">
-              <div class="col-md-4">
-                <h5 class="text-light">Email</h5>
-                <p class="text-white paragraph-lg font-secondary">steve.fruits@email.com</p>
-              </div>
-              <div class="col-md-4">
-                <h5 class="text-light">Phone</h5>
-                <p class="text-white paragraph-lg font-secondary">+880 2544 658 256</p>
-              </div>
-              <div class="col-md-4">
-                <h5 class="text-light">Address</h5>
-                <p class="text-white paragraph-lg font-secondary">125/A, CA Commercial Area, California, USA</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="border-top text-center border-dark py-5">
-          <p class="mb-0 text-light">Copyright &copy;<script>
-              var CurrentYear = new Date().getFullYear()
-              document.write(CurrentYear)
-            </script> Designed &amp; Developed by <a class="text-white-50" href="Themefisher">Themefisher</a></p>
-        </div>
-      </footer>
+  <footer class="bg-dark">
+    <div class="border-top text-center border-dark py-3">
+      <p class="mb-0 text-light">
+        Copyright &copy; {year}
+      </p>
+    </div>
+  </footer>
 )
 
 export default Footer
--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,37 +1,54 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { StaticQuery, graphql, Link } from "gatsby"
 
 const Header = ({ siteTitle }) => (
-  <header class="navigation fixed-top">
-    <nav class="navbar navbar-expand-lg navbar-dark">
-      <Link class="navbar-brand font-tertiary h3" to="/">
-        {siteTitle}
-      </Link>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation"
-        aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse text-center" id="navigation">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="index.html">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="about.html">about</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="blog.html">Blog</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="portfolio.html">Portfolio</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="contact.html">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
+  <StaticQuery 
+    query={graphql`
+        query {
+          allWpMenu(filter: {id: {eq: "dGVybToy"}}) {
+            edges {
+              node {
+                menuItems {
+                  nodes {
+                    label
+                  }
+                }
+              }
+            }
+          }
+        }
+    `}
+    render={data => (
+      <header class="navigation fixed-top">
+        <nav class="navbar navbar-expand-lg navbar-dark">
+          <Link class="navbar-brand font-tertiary h3" to="/">
+            {siteTitle}
+          </Link>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation"
+            aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse text-center" id="navigation">
+            <ul class="navbar-nav ml-auto">
+              <li className="nav-item"><Link className="nav-link" to={`/`}>Home</Link></li>
+              {data.allWpMenu.edges[0].node.menuItems.nodes.map(item => {
+                return (
+                  <li class="nav-item">
+                    <Link 
+                      class="nav-link" 
+                      to={`/${item.label.toLowerCase()}`}
+                    >
+                      {item.label}
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        </nav>
+      </header>
+    )} 
+    />
 )
 
 export default Header
